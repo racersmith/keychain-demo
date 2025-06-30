a72c3233b1237e8d74d8be4225699449bb122648
racersmith@gmail.com <racersmith@gmail.com>
2025-06-30 21:51:37 +0000

    Edited Pages.Polling

--- client_code/Pages/Polling/__init__.py
+++ client_code/Pages/Polling/__init__.py
@@ -2,6 +2,8 @@ from ._anvil_designer import PollingTemplate
 
 from keychain.client import fetch
 
+import json
+
 
 class Polling(PollingTemplate):
     def __init__(self, **properties):
@@ -11,6 +13,8 @@ class Polling(PollingTemplate):
 
     def timer_1_tick(self, **event_args):
         """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-        data = fetch(["first_load"], force_update=self.forced)
-        self.text_1.text = data
+        # first_load is part of global cache and will only be updated if forced.
+        # server_time is not part of global cache so it will always be requested from the server
+        data = fetch(["first_load", "server_time"], force_update=self.forced)
+        self.text_1.text = json.dumps(data, indent=2)
         self.heartbeat_icon.selected = ~self.heartbeat_icon.selected
