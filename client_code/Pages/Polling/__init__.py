330ebdf8e0e27979e83597a74f7d1babb61a4c0b
racersmith@gmail.com <racersmith@gmail.com>
2025-06-30 21:12:15 +0000

    Edited Pages.Polling and ClientData

--- client_code/Pages/Polling/__init__.py
+++ client_code/Pages/Polling/__init__.py
@@ -1,13 +1,16 @@
 from ._anvil_designer import PollingTemplate
 
-from keychain.client import fetch_from_server
+from keychain.client import fetch
 
 
 class Polling(PollingTemplate):
     def __init__(self, **properties):
+        self.forced = False
         self.init_components(**properties)
+        
 
     def timer_1_tick(self, **event_args):
         """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-        data = fetch_from_server("server_time")
+        data = fetch(["first_load"], force_update=self.forced)
         self.text_1.text = data
+        self.heartbeat_icon.selected = ~self.heartbeat_icon.selected
