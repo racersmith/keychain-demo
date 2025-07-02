166a2f4d6d09973a6285cb7b02b75db148effe18
racersmith@gmail.com <racersmith@gmail.com>
2025-07-02 14:36:33 +0000

    Edited ServerModule

--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -2,7 +2,10 @@ import anvil.server
 
 from keychain.client import invalidate
 
+from routing import router
+
 @anvil.server.callable
 def server_side_invalidate(field_or_key=None, *, path=None, auto_invalidate_paths=True):
     invalidate(field_or_key, path=path, auto_invalidate_paths=auto_invalidate_paths)
-    
+    if path:
+        router.invalidate(path=path)
