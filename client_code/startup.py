6ba99d2ba7a0169dd0f0f0d805931458e40249bd
racersmith@gmail.com <racersmith@gmail.com>
2025-07-07 17:35:31 +0000

    Edited startup

--- client_code/startup.py
+++ client_code/startup.py
@@ -1,9 +1,10 @@
 from routing.router import launch
-from .import routes
+from . import routes
 from keychain.client import initialize_cache
+
 
 if __name__ == "__main__":
     launch()
-    
+
     # This initializes
     initialize_cache()
